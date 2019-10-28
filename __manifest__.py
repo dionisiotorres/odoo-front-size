5f2cddc5221c75c53e3515ca158babd21a9bd6a0
inti VeeraNagaRajaSankar <47778391+intisankar@users.noreply.github.com>
2019-10-28 16:27:11 +0530

    font size
    The font sizes of the odoo12 framework are increased

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'odoo Fonts size',
+    'version': '1.0',
+    'category': 'core',
+    'sequence': 90,
+    'summary': 'Odoo 12 front end fonts are incresed',
+    'description': "",
+    'website': '',
+    'depends': [],
+    'data': ['font_size.xml',],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
